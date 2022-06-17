a4f47383bcc0916fc71cb89fe9f1fea8fca71842
Ailton cesar <ailtonc.n64@gmail.com>
2022-06-16 21:42:33 -0300

    initial commit

---
+++ app/src/main/java/com/example/appsorteio/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.appsorteio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sortearNumero(view: View) {
+        var texto = findViewById<TextView>(R.id.textView)
+        var numeroSorteado = Random.nextInt(11)
+        texto.text = "O número sorteado é: " + numeroSorteado
+    }
+}
